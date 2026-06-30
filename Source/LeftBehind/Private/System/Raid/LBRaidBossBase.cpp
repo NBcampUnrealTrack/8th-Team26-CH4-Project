a7585841b5919a8c09d982ad831907869ecf497b
Asera <kpo9101@gmail.com>
2026-06-30 21:04:14 +0900

    [add] 테스트 보스 추가

---
+++ Source/LeftBehind/Private/System/Raid/LBRaidBossBase.cpp
@@ -0,0 +1,83 @@
+﻿//LBRaidBossBase.cpp
+//테스트 보스 추가
+
+#include "System/Raid/LBRaidBossBase.h"
+
+#include "Net/UnrealNetwork.h"
+
+ALBRaidBossBase::ALBRaidBossBase()
+{
+	bReplicates = true;
+	SetReplicateMovement(true);
+}
+
+void ALBRaidBossBase::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(ALBRaidBossBase, CurrentHP);
+	DOREPLIFETIME(ALBRaidBossBase, MaxHP);
+	DOREPLIFETIME(ALBRaidBossBase, DEF);
+	DOREPLIFETIME(ALBRaidBossBase, bIsDead);
+}
+
+void ALBRaidBossBase::InitializeBossStats_ServerOnly(float InMaxHP, float InDEF)
+{
+	if (!HasAuthority())
+	{
+		return;
+	}
+
+	MaxHP = FMath::Max(1.f, InMaxHP);
+	CurrentHP = MaxHP;
+	DEF = FMath::Max(0.f, InDEF);
+	bIsDead = false;
+
+	OnRep_CurrentHP();
+	ForceNetUpdate();
+}
+
+void ALBRaidBossBase::ApplyRaidDamage_ServerOnly(float DamageAmount)
+{
+	if (!HasAuthority() || bIsDead)
+	{
+		return;
+	}
+
+	const float SafeDamage = FMath::Max(0.f, DamageAmount);
+	if (SafeDamage <= 0.f)
+	{
+		return;
+	}
+
+	CurrentHP = FMath::Max(0.f, CurrentHP - SafeDamage);
+
+	OnRep_CurrentHP();
+	ForceNetUpdate();
+
+	if (CurrentHP <= 0.f)
+	{
+		Die_ServerOnly();
+	}
+}
+
+void ALBRaidBossBase::Die_ServerOnly()
+{
+	if (!HasAuthority() || bIsDead)
+	{
+		return;
+	}
+
+	bIsDead = true;
+	CurrentHP = 0.f;
+
+	OnRep_CurrentHP();
+	OnBossDied.Broadcast();
+
+	ForceNetUpdate();
+}
+
+void ALBRaidBossBase::OnRep_CurrentHP()
+{
+	OnBossHPChanged.Broadcast(CurrentHP, MaxHP);
+}
---
+++ Source/LeftBehind/Public/System/Raid/LBRaidBossBase.h
@@ -0,0 +1,64 @@
+﻿//LBRaidBossBase.h
+//테스트 보스 추가
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "LBRaidBossBase.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnLBBossHPChangedSignature, float, CurrentHP, float, MaxHP);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnLBBossDiedSignature);
+
+UCLASS()
+class LEFTBEHIND_API ALBRaidBossBase : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	ALBRaidBossBase();
+
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	UPROPERTY(BlueprintAssignable, Category="LB|Boss")
+	FOnLBBossHPChangedSignature OnBossHPChanged;
+
+	UPROPERTY(BlueprintAssignable, Category="LB|Boss")
+	FOnLBBossDiedSignature OnBossDied;
+
+	UFUNCTION(BlueprintCallable, Category="LB|Boss")
+	void InitializeBossStats_ServerOnly(float InMaxHP, float InDEF);
+
+	UFUNCTION(BlueprintCallable, Category="LB|Boss")
+	void ApplyRaidDamage_ServerOnly(float DamageAmount);
+
+	UFUNCTION(BlueprintPure, Category="LB|Boss")
+	float GetCurrentHP() const { return CurrentHP; }
+
+	UFUNCTION(BlueprintPure, Category="LB|Boss")
+	float GetMaxHP() const { return MaxHP; }
+
+	UFUNCTION(BlueprintPure, Category="LB|Boss")
+	float GetDEF() const { return DEF; }
+
+	UFUNCTION(BlueprintPure, Category="LB|Boss")
+	bool IsDead() const { return bIsDead; }
+
+protected:
+	UPROPERTY(ReplicatedUsing=OnRep_CurrentHP, BlueprintReadOnly, Category="LB|Boss")
+	float CurrentHP = 10000.f;
+
+	UPROPERTY(Replicated, BlueprintReadOnly, Category="LB|Boss")
+	float MaxHP = 10000.f;
+
+	UPROPERTY(Replicated, BlueprintReadOnly, Category="LB|Boss")
+	float DEF = 50.f;
+
+	UPROPERTY(Replicated, BlueprintReadOnly, Category="LB|Boss")
+	bool bIsDead = false;
+
+	UFUNCTION()
+	void OnRep_CurrentHP();
+
+	void Die_ServerOnly();
+};
