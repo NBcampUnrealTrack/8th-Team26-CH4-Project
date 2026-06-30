f6159cbe6fc844dda21bd5b3395b20fc86bafee1
Asera <kpo9101@gmail.com>
2026-06-30 21:11:31 +0900

    [add] GameState 생

---
+++ Source/LeftBehind/Private/System/Raid/LBRaidGameState.cpp
@@ -0,0 +1,111 @@
+﻿//LBRaidGameState.cpp
+
+#include "System/Raid/LBRaidGameState.h"
+
+#include "Net/UnrealNetwork.h"
+
+ALBRaidGameState::ALBRaidGameState()
+{
+    bReplicates = true;
+}
+
+void ALBRaidGameState::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+    Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+    DOREPLIFETIME(ALBRaidGameState, RaidState);
+    DOREPLIFETIME(ALBRaidGameState, CountdownEndServerTime);
+    DOREPLIFETIME(ALBRaidGameState, BattleStartServerTime);
+    DOREPLIFETIME(ALBRaidGameState, TimeLimitSec);
+    DOREPLIFETIME(ALBRaidGameState, BossCurrentHP);
+    DOREPLIFETIME(ALBRaidGameState, BossMaxHP);
+    DOREPLIFETIME(ALBRaidGameState, RaidResult);
+}
+
+float ALBRaidGameState::GetCountdownRemaining() const
+{
+    if (RaidState != ELBRaidState::Countdown)
+    {
+        return 0.f;
+    }
+
+    return FMath::Max(0.f, CountdownEndServerTime - GetServerWorldTimeSeconds());
+}
+
+float ALBRaidGameState::GetBattleElapsed() const
+{
+    if (BattleStartServerTime <= 0.f)
+    {
+        return 0.f;
+    }
+
+    return FMath::Max(0.f, GetServerWorldTimeSeconds() - BattleStartServerTime);
+}
+
+float ALBRaidGameState::GetBattleRemaining() const
+{
+    if (RaidState != ELBRaidState::Battle)
+    {
+        return 0.f;
+    }
+
+    return FMath::Max(0.f, TimeLimitSec - GetBattleElapsed());
+}
+
+float ALBRaidGameState::GetBossHPRatio() const
+{
+    return BossMaxHP > 0.f ? BossCurrentHP / BossMaxHP : 0.f;
+}
+
+void ALBRaidGameState::SetRaidState_ServerOnly(ELBRaidState NewState)
+{
+    if (!HasAuthority())
+    {
+        return;
+    }
+
+    RaidState = NewState;
+    OnRep_RaidState();
+    ForceNetUpdate();
+}
+
+void ALBRaidGameState::SetBossHP_ServerOnly(float CurrentHP, float MaxHP)
+{
+    if (!HasAuthority())
+    {
+        return;
+    }
+
+    BossCurrentHP = FMath::Max(0.f, CurrentHP);
+    BossMaxHP = FMath::Max(1.f, MaxHP);
+
+    OnRep_BossHP();
+    ForceNetUpdate();
+}
+
+void ALBRaidGameState::SetRaidResult_ServerOnly(const FLBRaidResultData& NewResult)
+{
+    if (!HasAuthority())
+    {
+        return;
+    }
+
+    RaidResult = NewResult;
+    OnRep_RaidResult();
+    ForceNetUpdate();
+}
+
+void ALBRaidGameState::OnRep_RaidState()
+{
+    OnRaidStateChanged.Broadcast(RaidState);
+}
+
+void ALBRaidGameState::OnRep_BossHP()
+{
+    OnBossHPChanged.Broadcast(BossCurrentHP, BossMaxHP);
+}
+
+void ALBRaidGameState::OnRep_RaidResult()
+{
+    OnRaidResultChanged.Broadcast(RaidResult);
+}
---
+++ Source/LeftBehind/Public/System/Raid/LBRaidGameState.h
@@ -0,0 +1,79 @@
+﻿//LBRaidGameState.h
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameStateBase.h"
+#include "System/Raid/LBRaidTypes.h"
+#include "LBRaidGameState.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnLBRaidStateChanged, ELBRaidState, NewState);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnLBBossHPChanged, float, CurrentHP, float, MaxHP);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnLBRaidResultChanged, const FLBRaidResultData&, ResultData);
+
+UCLASS()
+class LEFTBEHIND_API ALBRaidGameState : public AGameStateBase
+{
+    GENERATED_BODY()
+
+public:
+    ALBRaidGameState();
+
+    virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+    UPROPERTY(BlueprintAssignable)
+    FOnLBRaidStateChanged OnRaidStateChanged;
+
+    UPROPERTY(BlueprintAssignable)
+    FOnLBBossHPChanged OnBossHPChanged;
+
+    UPROPERTY(BlueprintAssignable)
+    FOnLBRaidResultChanged OnRaidResultChanged;
+
+    UPROPERTY(ReplicatedUsing=OnRep_RaidState, BlueprintReadOnly, Category="LB|Raid")
+    ELBRaidState RaidState = ELBRaidState::Waiting;
+
+    UPROPERTY(Replicated, BlueprintReadOnly, Category="LB|Raid")
+    float CountdownEndServerTime = 0.f;
+
+    UPROPERTY(Replicated, BlueprintReadOnly, Category="LB|Raid")
+    float BattleStartServerTime = 0.f;
+
+    UPROPERTY(Replicated, BlueprintReadOnly, Category="LB|Raid")
+    float TimeLimitSec = 300.f;
+
+    UPROPERTY(ReplicatedUsing=OnRep_BossHP, BlueprintReadOnly, Category="LB|Raid")
+    float BossCurrentHP = 0.f;
+
+    UPROPERTY(ReplicatedUsing=OnRep_BossHP, BlueprintReadOnly, Category="LB|Raid")
+    float BossMaxHP = 1.f;
+
+    UPROPERTY(ReplicatedUsing=OnRep_RaidResult, BlueprintReadOnly, Category="LB|Raid")
+    FLBRaidResultData RaidResult;
+
+    UFUNCTION(BlueprintPure, Category="LB|Raid")
+    float GetCountdownRemaining() const;
+
+    UFUNCTION(BlueprintPure, Category="LB|Raid")
+    float GetBattleElapsed() const;
+
+    UFUNCTION(BlueprintPure, Category="LB|Raid")
+    float GetBattleRemaining() const;
+
+    UFUNCTION(BlueprintPure, Category="LB|Raid")
+    float GetBossHPRatio() const;
+
+    void SetRaidState_ServerOnly(ELBRaidState NewState);
+    void SetBossHP_ServerOnly(float CurrentHP, float MaxHP);
+    void SetRaidResult_ServerOnly(const FLBRaidResultData& NewResult);
+
+protected:
+    UFUNCTION()
+    void OnRep_RaidState();
+
+    UFUNCTION()
+    void OnRep_BossHP();
+
+    UFUNCTION()
+    void OnRep_RaidResult();
+};
