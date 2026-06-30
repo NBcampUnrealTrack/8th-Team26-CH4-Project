43fcf64bdc57e54887a2b9a4906a2c7248300cf3
Asera <kpo9101@gmail.com>
2026-06-30 20:05:18 +0900

    [add] LBRaidTypes.h 생성

---
+++ Source/LeftBehind/Public/System/Raid/LBRaidTypes.h
@@ -0,0 +1,48 @@
+﻿//LBRaidTypes.h
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "LBRaidTypes.generated.h"
+
+UENUM(BlueprintType)
+enum class ELBRaidState : uint8
+{
+	Waiting     UMETA(DisplayName="Waiting"),
+	Countdown   UMETA(DisplayName="Countdown"),
+	Battle      UMETA(DisplayName="Battle"),
+	Result      UMETA(DisplayName="Result")
+};
+
+UENUM(BlueprintType)
+enum class ELBRaidEndReason : uint8
+{
+	None        UMETA(DisplayName="None"),
+	BossKilled  UMETA(DisplayName="BossKilled"),
+	AllDead     UMETA(DisplayName="AllDead"),
+	TimeOut     UMETA(DisplayName="TimeOut")
+};
+
+USTRUCT(BlueprintType)
+struct FLBRaidResultData
+{
+	GENERATED_BODY()
+
+	UPROPERTY(BlueprintReadOnly)
+	bool bVictory = false;
+
+	UPROPERTY(BlueprintReadOnly)
+	ELBRaidEndReason EndReason = ELBRaidEndReason::None;
+
+	UPROPERTY(BlueprintReadOnly)
+	float ClearTimeSec = 0.f;
+
+	UPROPERTY(BlueprintReadOnly)
+	FName RankID = NAME_None;
+
+	UPROPERTY(BlueprintReadOnly)
+	int32 PlayerDeaths = 0;
+
+	UPROPERTY(BlueprintReadOnly)
+	float BossRemainingHPOnFail = 0.f;
+};
