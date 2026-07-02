120f5866457fd958b07338fb06b67f5358b7a4e3
KWON <mario2368@Naver.com>
2026-07-02 21:00:36 +0900

    fix: 태그 수정

--- Source/LeftBehind/Private/GameplayTags/LBTags.cpp
+++ Source/LeftBehind/Private/GameplayTags/LBTags.cpp
@@ -4,6 +4,7 @@ namespace LBTags
 {
 	namespace LBAbilities
 	{
+		UE_DEFINE_GAMEPLAY_TAG_COMMENT(Primary, "LBTags.LBAbilities.Primary", "Tag for the Primary Ability");
 		UE_DEFINE_GAMEPLAY_TAG_COMMENT(ActivateOnGiven,"CCTags.CCAbilities.ActivateOnGiven","Tag for the Abilities that sould activate immediately");
 	}
 	
