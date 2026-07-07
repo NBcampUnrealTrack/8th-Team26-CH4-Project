2b3a5584e390c3b6263c6a7097cdd7c7ab3f2dd3
KWON <mario2368@Naver.com>
2026-07-07 10:52:09 +0900

    feat: 이펙트, 머티리얼 에셋 추가

---
+++ Source/LeftBehind/Private/GameObjects/LB_Projectile.cpp
@@ -0,0 +1,33 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GameObjects/LB_Projectile.h"
+#include "Characters/LB_PlayerCharacter.h"
+#include "AbilitySystem/LB_AbilitySystemComponent.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+
+
+// Sets default values
+ALB_Projectile::ALB_Projectile()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+	ProjectileMovement = CreateDefaultSubobject<UProjectileMovementComponent>("ProjectileMovement");
+}
+
+void ALB_Projectile::NotifyActorBeginOverlap(AActor* OtherActor)
+{
+	Super::NotifyActorBeginOverlap(OtherActor);
+	
+	ALB_PlayerCharacter* PlayerCharacter = Cast<ALB_PlayerCharacter>(OtherActor);
+	if (!IsValid(PlayerCharacter) && !PlayerCharacter->IsAlive()) return;
+	
+	UAbilitySystemComponent* AbilitySystemComponent = PlayerCharacter->GetAbilitySystemComponent();
+	if (!IsValid(AbilitySystemComponent) || !HasAuthority()) return;
+	
+	FGameplayEffectContextHandle ContextHandle = AbilitySystemComponent->MakeEffectContext();
+	FGameplayEffectSpecHandle SpecHandle = AbilitySystemComponent->MakeOutgoingSpec(DamageEffect, 1.f, ContextHandle);
+}
+
+
+
---
+++ Source/LeftBehind/Public/GameObjects/LB_Projectile.h
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "LB_Projectile.generated.h"
+
+
+class UProjectileMovementComponent;
+class UGameplayEffect;
+
+UCLASS()
+class LEFTBEHIND_API ALB_Projectile : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ALB_Projectile();
+	virtual void NotifyActorBeginOverlap(AActor* OtherActor);
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "LB|Heal", meta = (ExposeOnSpawn, ClampMin = "0.0"))
+	float Heal{10.f};
+
+protected:
+	UPROPERTY(VisibleAnywhere, Category = "LB|Projectile")
+	TObjectPtr<UProjectileMovementComponent> ProjectileMovement;
+	
+	UPROPERTY(EditAnywhere, Category = "LB|Heal")
+	TSubclassOf<UGameplayEffect> DamageEffect;
+};
