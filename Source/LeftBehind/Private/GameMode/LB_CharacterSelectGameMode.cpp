29f9cffea126cc934a4a5a4018aa21389ee5db2a
bjt <dkvldzm@gamil.com>
2026-07-20 01:31:57 +0900

    [update] 보스 등작 컷씬 수정중

--- Source/LeftBehind/Private/GameMode/LB_CharacterSelectGameMode.cpp
+++ Source/LeftBehind/Private/GameMode/LB_CharacterSelectGameMode.cpp
@@ -24,7 +24,7 @@ ALB_CharacterSelectGameMode::ALB_CharacterSelectGameMode()
 	HUDClass = nullptr;
 
 	RaidMap = TSoftObjectPtr<UWorld>(FSoftObjectPath(
-		TEXT("/Game/LeftBehind/Maps/Blockout/Lv_Boss1_Blockout.Lv_Boss1_Blockout")));
+		TEXT("/Game/LeftBehind/Maps/Lv_Boss")));
 }
 
 void ALB_CharacterSelectGameMode::BeginPlay()
